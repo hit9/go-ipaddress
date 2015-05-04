634796e396e292e7e8ab8fc21b7fa02f7b51d422
hit9 <hit9@icloud.com>
2015-05-04 23:08:34 +0800

    Update

--- ipv4/ipv4.go
+++ ipv4/ipv4.go
@@ -1,47 +1,55 @@
+// IPv4 address utils for golang.
+// Copyright 2015. Chao Wang <hit9@icloud.com>
+
 package ipv4
 
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"strconv"
 	"strings"
 )
 
-func Atoi(address string) (integer uint32, err error) {
+// Atoi returns the uint32 representation of an ipv4 address string value.
+// Example:
+//
+//		Atoi("192.168.0.1")   // 3232312315
+func Atoi(address string) (uint32, error) {
+	var sum uint32 = 0
+
 	if len(address) > 15 {
-		return integer, errors.New("invalid address string")
+		return sum, errors.New("address too long")
 	}
 
 	octs := strings.Split(address, ".")
 
 	if len(octs) != 4 {
-		return integer, errors.New("4 octects required")
+		return sum, errors.New("requires 4 octects")
 	}
 
 	for i := 0; i < 4; i++ {
-		oct, err := strconv.Atoi(octs[i])
-
+		oct, err := strconv.ParseUint(octs[i], 10, 0)
 		if err != nil {
-			return integer, fmt.Errorf("bad octect: %s", octs[i])
+			return sum, errors.New("bad octect " + octs[i])
 		}
-
-		integer += uint32(oct) << uint32((4 - 1 - i) * 8)
+		sum += uint32(oct) << uint32((4-1-i)*8)
 	}
-	return
+	return sum, nil
 }
 
-func Itoa(integer uint32) (address string, err error) {
+// Itoa returns the string representation of an ipv4 address uint32 value.
+// Example:
+//
+//		Itoa(3232312315)  // "192.168.0.1"
+func Itoa(integer uint32) string {
 	var buf bytes.Buffer
 
 	for i := 0; i < 4; i++ {
-		oct := (integer >> uint32((4 - 1 - i) * 8)) & 0xff
-		buf.WriteString(strconv.Itoa(int(oct)))
-
+		oct := (integer >> uint32((4-1-i)*8)) & 0xff
+		buf.WriteString(strconv.FormatUint(uint64(oct), 10))
 		if i < 3 {
-			buf.WriteByte(46)
+			buf.WriteByte('.')
 		}
-
 	}
-	return buf.String(), err
+	return buf.String()
 }
