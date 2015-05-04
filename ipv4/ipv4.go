3b5551073821d2961e798de2d364ff652926fa11
hit9 <hit9@icloud.com>
2015-05-04 23:58:46 +0800

    Add readme

--- ipv4/ipv4.go
+++ ipv4/ipv4.go
@@ -1,4 +1,4 @@
-// IPv4 address utils for golang.
+// IPv4 addr utils for golang.
 // Copyright 2015. Chao Wang <hit9@icloud.com>
 
 package ipv4
@@ -10,24 +10,20 @@ import (
 	"strings"
 )
 
-// Atoi returns the uint32 representation of an ipv4 address string value.
+// Atoi returns the uint32 representation of an ipv4 addr string value.
 // Example:
 //
 //	Atoi("192.168.0.1")   // 3232312315
 //
-func Atoi(address string) (uint32, error) {
+func Atoi(addr string) (uint32, error) {
 	var sum uint32 = 0
-
-	if len(address) > 15 {
-		return sum, errors.New("address too long")
+	if len(addr) > 15 {
+		return sum, errors.New("addr too long")
 	}
-
-	octs := strings.Split(address, ".")
-
+	octs := strings.Split(addr, ".")
 	if len(octs) != 4 {
 		return sum, errors.New("requires 4 octects")
 	}
-
 	for i := 0; i < 4; i++ {
 		oct, err := strconv.ParseUint(octs[i], 10, 0)
 		if err != nil {
@@ -38,14 +34,13 @@ func Atoi(address string) (uint32, error) {
 	return sum, nil
 }
 
-// Itoa returns the string representation of an ipv4 address uint32 value.
+// Itoa returns the string representation of an ipv4 addr uint32 value.
 // Example:
 //
 //	Itoa(3232312315)  // "192.168.0.1"
 //
 func Itoa(integer uint32) string {
 	var buf bytes.Buffer
-
 	for i := 0; i < 4; i++ {
 		oct := (integer >> uint32((4-1-i)*8)) & 0xff
 		buf.WriteString(strconv.FormatUint(uint64(oct), 10))
@@ -55,3 +50,43 @@ func Itoa(integer uint32) string {
 	}
 	return buf.String()
 }
+
+// Example:
+//
+//	Not("0.0.255.255")  // "255.255.0.0"
+//
+func Not(addr string) (string, error) {
+	i, err := Atoi(addr)
+	return Itoa(i ^ 0xffffffff), err
+}
+
+// Example:
+//
+//	Or("0.0.1.1", "1.1.0.0")  // "1.1.1.1"
+//
+func Or(addra string, addrb string) (string, error) {
+	ia, err := Atoi(addra)
+	ib, err := Atoi(addrb)
+	return Itoa(ia | ib), err
+}
+
+// Example:
+//
+//	Xor("0.255.255.255", "192.255.255.255")  // "192.0.0.0"
+//
+func Xor(addra string, addrb string) (string, error) {
+	ia, err := Atoi(addra)
+	ib, err := Atoi(addrb)
+	return Itoa(ia ^ ib), err
+}
+
+// Example:
+//
+//	Next("192.168.0.1")  // "192.168.0.2"
+//
+func Next(addr string) (string, error) {
+	i, err := Atoi(addr)
+	return Itoa(i + 1)
+}
+
+
